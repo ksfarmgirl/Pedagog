2d818cb38dd86ad021e7e9c647c2b62207254177
Laura Seiler <lseiler@ksu.edu>
2022-11-11 10:08:14 -0600

    Updated html with header

--- dist/extension.js
+++ dist/extension.js
@@ -3269,12 +3269,13 @@ function activate(context) {
     return `<!DOCTYPE html>
     <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Debugger</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Debugger</title>
     </head>
     <body>
-        <p>Hello Worldddd</p>
+      <h1>Pedagogical IDE</h1>
+      <p>Python, C#, and Java debugger</p>
     </body>
     </html>`;
   }
