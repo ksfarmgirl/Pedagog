d20055af37e62b7aef46ac075ebfa7fd55ef5a49
Wes Steiner <wwes@ksu.edu>
2022-12-05 13:27:44 -0600

    Confirmed Function of Python Extension

--- sampleWorkspace/test.py
+++ sampleWorkspace/test.py
@@ -1,9 +1,1 @@
-def listSum(numbers):
-  if not numbers:
-    return 0
-  else:
-    (f, rest) = numbers
-    return f + listSum(rest)
-
-myList = (1, (2, (3, None)))
-total = listSum(myList)
+print('Hello world!')
